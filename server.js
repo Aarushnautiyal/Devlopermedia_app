a63dacc8b003e24a7fe128837ece6f9dafac9da2
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-05-12 16:30:02 +0530

    production changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const connectDB = require("./config/db")
 const mongoose = require('mongoose')
 const app = express()
+const path = require('path')
 
 // connecting db
 
@@ -19,6 +20,15 @@ app.use("/api/auth",require('./routes/api/auth'))
 app.use("/api/profile",require('./routes/api/profile'))
 app.use("/api/post",require('./routes/api/post'))
 
+//serve static asset in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT||5000
 
